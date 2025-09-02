7adb04ca970daf5e82f1b9dea5ddab43781a6a0e
Nandyala Jashwanth Reddy <cybersafe07@gmail.com>
2025-09-02 22:11:46 +0530

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-  // Add other reducers here as needed
-});
-
-export default rootReducer;
